84174eb1a3e5361af5c518fb8bc1d10bed91e26d
Michael Welch <michaelwelxh@gmail.com>
2026-08-16 18:15:52 +0100

    feat: mostly finnshed multi-threaded C++ program (need to set up vs)

--- cpp/ex1/main.cpp
+++ cpp/ex1/main.cpp
@@ -29,37 +29,54 @@ void sum_partial(const std::vector<int>& arr, int start, int end) {
         local_sum += arr[i];
     }
     // lock
+    std::lock_guard<std::mutex> lock(mtx);
     // add to the global sum 
+    global_sum += global_sum
 }
 
 int main() {
     // define arr sicxe 
     const int SIZE = 100'000'000;
-    // deifne number of threads
-    const int NUM_THREADS = ....
+    // deifne number of threads -> hardware_concurrency will decice 
+    const int NUM_THREADS = std::thread::hardware_concurrency();
 
     std::vector<int> arr(SIZE);
     // https://en.cppreference.com/cpp/algorithm/iota 
     std::iota(arr.begin(), arr.end(), -100);
 
     // start the timer 
+    auto start_time = std::chrono::high_resolution_clock::now()
+
+
+    // create the thread      
     std::vector<std::thread> threads;
     // partirction the array for each of the threads
     int part = SIZE / NUM_THREADS;
 
-    // THREAD CALLING LOGIC 
-        // create the thread      
+    // THREAD CALLING LOGIC -> call threads to compute there part
+    for (int i = 0; i < NUM_THREADS; i++) {
         // call each of the threads ---- need to learn how to do this
+        int start = i * chunk;
+        int end = (i == NUM_THREADS - 1) ? SIZE : start + chunk;
+        threads.emplace_back(sum_partial, std::ref(arr), start, end);
+    }
+
+    // what for threads to finnish computaion
+    for (&auto t : threads) {
+        // join 
+        t.join()
+    }
 
     // END TIME 
+    auto end_time = std::chrono::high_resolution_clock::now()
+
     // CALCULAETE DURATION 
+    auto dureation = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
     // OUPTUT
-    std::count << "Sum " <<... <<  std::..;
-    std::count << "Time " <<... << " ms"  std::...;
-    std::count << "Duration " <<... << std::...;
+    std::count << "Sum " << global_sum <<  std::endl;  
+    std::count << "Time " << duration.count() << " ms"  std::endl;
+    std::count << "Threads " << NUM_THREADS << std::endl;
 
     return 0;
-
-
 }
