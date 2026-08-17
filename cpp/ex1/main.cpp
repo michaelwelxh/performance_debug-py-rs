0193f044bdba4bb14d47590b3bb3a048000af06f
Michael Welch <michaelwelxh@gmail.com>
2026-08-17 20:50:31 +0100

    feat: compile/run c++, rust code against local go server

--- cpp/ex1/main.cpp
+++ cpp/ex1/main.cpp
@@ -11,16 +11,17 @@ sum a large array in parallel; profile it with perf .
 #include <chrono>
 #include <numeric>
 
-
-/*
-need to learn chrono, mutex and thread -> the rest is simple enough to guess through logic i think will be similar to last java concurreny project -> call thread lock and run join not sure on the syntax though
-
-*/
-
-
 std::mutex mtx;
 long long global_sum = 0;
 
+
+/*
+Set up visual studios 
+
+Sum 4999989950000000
+Time 2181 ms
+Threads 12
+*/
 
 void sum_partial(const std::vector<int>& arr, int start, int end) {
     // assign (longlong 64bit      -9.22 x 10^18 to +9.22 x 10^18)
@@ -31,52 +32,53 @@ void sum_partial(const std::vector<int>& arr, int start, int end) {
     // lock
     std::lock_guard<std::mutex> lock(mtx);
     // add to the global sum 
-    global_sum += global_sum
+    global_sum += local_sum;
 }
 
 int main() {
     // define arr sicxe 
     const int SIZE = 100'000'000;
     // deifne number of threads -> hardware_concurrency will decice 
-    const int NUM_THREADS = std::thread::hardware_concurrency();
+    unsigned int NUM_THREADS = std::thread::hardware_concurrency();
+    if (NUM_THREADS == 0) NUM_THREADS = 4;
 
     std::vector<int> arr(SIZE);
     // https://en.cppreference.com/cpp/algorithm/iota 
-    std::iota(arr.begin(), arr.end(), -100);
+    std::iota(arr.begin(), arr.end(), 0);
 
     // start the timer 
-    auto start_time = std::chrono::high_resolution_clock::now()
+    auto start_time = std::chrono::high_resolution_clock::now();
 
 
     // create the thread      
     std::vector<std::thread> threads;
     // partirction the array for each of the threads
-    int part = SIZE / NUM_THREADS;
+    int chunk = SIZE / NUM_THREADS;
 
     // THREAD CALLING LOGIC -> call threads to compute there part
     for (int i = 0; i < NUM_THREADS; i++) {
         // call each of the threads ---- need to learn how to do this
         int start = i * chunk;
         int end = (i == NUM_THREADS - 1) ? SIZE : start + chunk;
-        threads.emplace_back(sum_partial, std::ref(arr), start, end);
+        threads.emplace_back(sum_partial, arr, start, end);
     }
 
     // what for threads to finnish computaion
-    for (&auto t : threads) {
+    for (auto& t : threads) {
         // join 
-        t.join()
+        t.join();
     }
 
     // END TIME 
-    auto end_time = std::chrono::high_resolution_clock::now()
+    auto end_time = std::chrono::high_resolution_clock::now();
 
     // CALCULAETE DURATION 
-    auto dureation = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
     // OUPTUT
-    std::count << "Sum " << global_sum <<  std::endl;  
-    std::count << "Time " << duration.count() << " ms"  std::endl;
-    std::count << "Threads " << NUM_THREADS << std::endl;
+    std::cout << "Sum " << global_sum <<  std::endl;  
+    std::cout << "Time " << duration.count() << " ms" << std::endl;
+    std::cout << "Threads " << NUM_THREADS << std::endl;
 
     return 0;
 }
--- rs/ex2/src/main.rs
+++ rs/ex2/src/main.rs
@@ -5,37 +5,40 @@
 // -> need to set up rust on pc  ->>>>> MSVC build tools do tommorow
 
 
-use tokio::time::{sleep, Duration};
+// use tokio::time::{sleep, Duration};
 use reqwest;
 
 
 async fn fetch(url: &str) -> Result<String, reqwest::Error> {
     // responce
-    let responce =  
+    let responce = reqwest::get(url).await;
     // body 
-    let body = 
-    
-    Ok(body)
+    let body = responce?.text().await;
+
+    Ok(body?)
 }
 
 // define the attribute tag 
 #[tokio::main]
 // making sure to mark the function as async
 async fn main() -> Result<(), reqwest::Error> {
-    //  spawn 3 conmcurrnet tasks
-    let task1 = tokio::spawn(fetch("https://example.com"));
-    let task1 = tokio::spawn(fetch("https://example.org"));
-    let task1 = tokio::spawn(fetch("https://example.net"));
+    //  spawn 3 conmcurrnet tasks - feting from go server
+    let task1 = tokio::spawn(fetch("http://localhost:8090/hello"));
+    let task2 = tokio::spawn(fetch("http://localhost:8090/blah"));
+    let task3 = tokio::spawn(fetch("http://localhost:8090/by"));
 
     // await the task finnifng 
     let (res1, res2, res3) = tokio::try_join!(task1, task2, task3).unwrap();
 
-    println!("1: {} bytes", res1??.len());
-    println!("2: {} bytes", res2??.len());
-    println!("3: {} bytes", res1??.len());
+    println!("1: {} bytes", res1?.len());
+    println!("2: {} bytes", res2?.len());
+    println!("3: {} bytes", res3?.len());
+    
+    // 1: 6 bytes     -> hello\n 
+    // 2: 5 bytes     -> blah\n
+    // 3: 3 bytes     -> by\n
+    // from concurrent server project go sever 
+
 
     Ok(())
 }
-
-
-// call main 
